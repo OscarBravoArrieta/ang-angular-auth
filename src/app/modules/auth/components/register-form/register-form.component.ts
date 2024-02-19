f74431fbdae1b6de277ba7da03b5da68515730e2
oscarbravo <oscarbravoarrieta@gmail.com>
2024-02-19 17:27:16 -0500

    Register module

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -1,40 +1,70 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+ import { Component, inject } from '@angular/core'
+ import { FormBuilder, Validators } from '@angular/forms'
+ import { Router } from '@angular/router'
+ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
-import { CustomValidators } from '@utils/validators';
 
-@Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-})
-export class RegisterFormComponent {
-  form = this.formBuilder.nonNullable.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
-    confirmPassword: ['', [Validators.required]],
-  }, {
-    validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
-  });
-  status: string = 'init';
-  faEye = faEye;
-  faEyeSlash = faEyeSlash;
-  showPassword = false;
+ import { CustomValidators } from '@utils/validators'
+ import { RequestStaus } from '@models/request-status.model'
+ import { AuthService } from '@services/auth.service'
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+ @Component({
+     selector: 'app-register-form',
+     templateUrl: './register-form.component.html',
+ })
+ export class RegisterFormComponent {
 
-  register() {
-    if (this.form.valid) {
-      this.status = 'loading';
-      const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
-    } else {
-      this.form.markAllAsTouched();
-    }
-  }
+     form = this.formBuilder.nonNullable.group({
+         name: ['', [Validators.required]],
+         email: ['', [Validators.email, Validators.required]],
+         password: ['', [Validators.minLength(8), Validators.required]],
+         confirmPassword: ['', [Validators.required]],
+     }, {
+         validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
+     })
+
+     // -------------------------------------------------------------------------------------------
+
+     status: string = 'init'
+     faEye = faEye
+     faEyeSlash = faEyeSlash
+     showPassword = false
+
+     private authService = inject(AuthService)
+     private router = inject(Router)
+
+
+     // -------------------------------------------------------------------------------------------
+
+     constructor(
+         private formBuilder: FormBuilder,
+
+     ) {}
+
+     // -------------------------------------------------------------------------------------------
+
+     register() {
+
+         if (this.form.valid) {
+             this.status = 'loading'
+             const { name, email, password } = this.form.getRawValue()
+             this.authService.register(name, email, password)
+             .subscribe({
+                 next: () => {
+                     this.status = 'succes'
+                     this.router.navigate(['/login'])
+                 },
+                 error: () => {
+                     this.status = 'error'
+
+                 }
+             })
+             console.log(name, email, password)
+         } else {
+             this.form.markAllAsTouched()
+        }
+
+     }
+
+     // -------------------------------------------------------------------------------------------
 }
